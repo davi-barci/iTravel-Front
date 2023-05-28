d71a6d381c806b2c9101f8fed81f6ab9d1b92087
Davi Barci <davibarci0208@gmail.com>
2023-05-28 16:03:41 -0300

    feat: add hosting search page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./pages/HomePage/Home";
 import NavBar from "./components/NavBar/NavBar";
 import Footer from "./components/Footer/Footer";
 import FlightsSearch from "./pages/Flights/ FlightsSearch";
+import HostingSearch from "./pages/Hosting/HostingSearch";
 
 register();
 
@@ -14,6 +15,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/flights" element={<FlightsSearch/>} />
+          <Route path="/hosting" element={<HostingSearch/>} />
         </Routes>
         <Footer/>
     </BrowserRouter>
--- src/pages/Hosting/HostingSearch.js
+++ src/pages/Hosting/HostingSearch.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import {BsAirplaneFill, BsFillBuildingsFill} from "react-icons/bs";
 import { useState } from "react";
 
-export default function FlightsSearch(){
+export default function HostingSearch(){
     const [minValue, setMinValue] = useState(0);
     const [maxValue, setMaxValue] = useState(10000);
 
@@ -15,10 +15,10 @@ export default function FlightsSearch(){
     };
 
     return(
-        <ContainerFlight>
+        <ContainerHosting>
             <div>
                 <form>
-                    <label>Passagens Aéreas</label>
+                    <label>Hospedagens</label>
                     <label>Origem</label>
                     <select name="cities">
                         <option value="" disabled selected>Selecione a sua origem</option>
@@ -62,41 +62,30 @@ export default function FlightsSearch(){
 
                 <div>
                     <div>
+                        <img/>
                         <div>
+                            <p>Nome Hotel Teste</p>
+                            <p>Rua dos bobos, 420, Rio de Janeiro</p>
                             <div>
-                                <img src="https://br.staticontent.com/flights-images/21.94.2/common/airlines/25x25/L0.png"/>
-                                <p>LATAM</p>
+                                <p>4.7</p>
                             </div>
                             <div>
-                                <p>28/05/23</p>
-                                <p>15:00</p>
-                                <p>GRU</p>
-                                <p>São Paulo</p>
-                            </div>
-                            <div>
-                                <p>Duração</p>
-                                <p>1h 15m</p>
-                            </div>
-                            <div>
-                                <p>28/05/23</p>
-                                <p>16:15</p>
-                                <p>SDU</p>
-                                <p>Rio de Janeiro</p>
                             </div>
                         </div>
                         <div>
-                            <p>Preço</p>
-                            <p>R$ 530</p>
-                            <button>Ver Mais</button>
+                            <p>Preço Diária</p>
+                            <p>R$ 2.280</p>
+                            <button>Veja Mais</button>
                         </div>
                     </div>
                 </div>
             </div>
-        </ContainerFlight>
+        </ContainerHosting>
     );
 }
 
-const ContainerFlight = styled.div`
+
+const ContainerHosting = styled.div`
     width: 100%;
     min-height: 1000px;
     height: auto;
@@ -237,7 +226,7 @@ const ContainerFlight = styled.div`
             >div:nth-of-type(1){
                 width: 50px;
                 height: 100%;
-                background-color: #2A95AE;
+                background-color: white;
                 border-top-left-radius: 10px;
                 border-bottom-left-radius: 10px;
                 border: 1px solid #CCCCCC;
@@ -249,7 +238,7 @@ const ContainerFlight = styled.div`
 
                 >svg{
                     font-size: 16px;
-                    color: white;
+                    color: grey;
                 }
 
                 >p{
@@ -263,7 +252,7 @@ const ContainerFlight = styled.div`
             >div:nth-of-type(2){
                 width: 50px;
                 height: 100%;
-                background-color: white;
+                background-color: #2A95AE;
                 border-top-right-radius: 10px;                      
                 border-bottom-right-radius: 10px;
                 border: 1px solid #CCCCCC;
@@ -275,7 +264,7 @@ const ContainerFlight = styled.div`
 
                 >svg{
                     font-size: 16px;
-                    color: grey;
+                    color: white;
                 }
 
                 >p{
@@ -297,10 +286,11 @@ const ContainerFlight = styled.div`
 
             >div{
                 width: 100%;
-                height: 160px;
-                background-color: white;
-                border-radius: 15px;
+                height: 250px;
                 display: flex;
+                background-color: white;
+                border: 1px solid #CCCCCC;
+                border-radius: 10px;
 
                 :hover{
                     -webkit-box-shadow: 10px 10px 3px -8px rgba(0,0,0,0.3);
@@ -308,107 +298,78 @@ const ContainerFlight = styled.div`
                     box-shadow: 10px 10px 3px -8px rgba(0,0,0,0.3);
                 }
 
-                >div:nth-of-type(1){
-                    width: 600px;
+                >img{
+                    width: 300px;
                     height: 100%;
-                    background-color: #e3e8f2;
-                    border-right: 4px dashed #CCCCCC;
-                    --mask: radial-gradient(20px at 100% 20px,#0000 98%,#000) 0 -20px;
-                    -webkit-mask: var(--mask);
-                    mask: var(--mask);
+                    object-fit: cover;
+                    border-top-left-radius: 10px;
+                    border-bottom-left-radius: 10px;
+                }
+
+                >div:nth-of-type(1){
+                    width: 350px;
+                    height: 100%;
                     display: flex;
-                    
+                    flex-direction: column;
+                    border-right: 1px solid #CCCCCC;
+
+                    >p:nth-of-type(1){
+                        font-family: 'Poppins', sans-serif;
+                        font-size: 20px;
+                        font-weight: 700;
+                        color: #2E2D33;
+                        margin-bottom: 5px;
+                        margin-top: 20px;
+                        margin-left: 10px;
+                    }
+
+                    >p:nth-of-type(2){
+                        font-family: 'Poppins', sans-serif;
+                        font-size: 12px;
+                        font-weight: 700;
+                        color: #2E2D33;
+                        margin-bottom: 10px;
+                        margin-left: 10px;
+                    }
+
                     >div:nth-of-type(1){
+                        width: 25px;
+                        height: 25px;
+                        border-radius: 5px;
+                        background-color: green;
                         display: flex;
-                        flex-direction: column;
-                        gap: 20px 0px;
                         justify-content: center;
                         align-items: center;
-                        width: 150px;
-                        height: 100%;
-                        border-right: 1px solid #CCCCCC;
-
-                        >img{
-                            width: 30px;
-                            height: 30px;
-                            object-fit: cover;
-                        }
+                        margin-bottom: 50px;
+                        margin-left: 10px;
 
                         >p{
                             font-family: 'Poppins', sans-serif;
-                            font-size: 16px;
+                            font-size: 12px;
                             font-weight: 700;
-                            color: #2E2D33;
+                            color: white;
                         }
                     }
 
                     >div:nth-of-type(2){
-                        display: flex;
-                        flex-direction: column;
-                        gap: 20px 0px;
-                        justify-content: center;
-                        align-items: center;
-                        width: 150px;
-                        height: 100%;
-
-                        >p{
-                            font-family: 'Poppins', sans-serif;
-                            font-size: 14px;
-                            font-weight: 700;
-                            color: #2E2D33;
-                        }
-                    }
-
-                    >div:nth-of-type(3){
-                        display: flex;
-                        flex-direction: column;
-                        justify-content: center;
-                        align-items: center;
-                        width: 150px;
-                        height: 100%;
-                        gap: 10px 0px;
-
-                        >p{
-                            color: #42A791;
-                            font-family: 'Poppins', sans-serif;
-                            font-size: 14px;
-                            font-weight: 700;
-                        }
-                    }
-
-                    >div:nth-of-type(4){
-                        display: flex;
-                        flex-direction: column;
-                        gap: 20px 0px;
-                        justify-content: center;
-                        align-items: center;
-                        width: 150px;
-                        height: 100%;
-
-                        >p{
-                            font-family: 'Poppins', sans-serif;
-                            font-size: 14px;
-                            font-weight: 700;
-                            color: #2E2D33;
-                        }
+                        width: 220px;
+                        height: 25px;
+                        background-color: black;
+                        margin-left: 10px;
                     }
                 }
 
                 >div:nth-of-type(2){
-                    width: 220px;
+                    width: 170px;
                     height: 100%;
-                    background-color: #e3e8f2;
                     display: flex;
                     flex-direction: column;
                     align-items: center;
                     justify-content: center;
-                    --mask: radial-gradient(20px at 0 20px,#0000 98%, #F5F7FB) 0 -20px;
-                    -webkit-mask: var(--mask);
-                            mask: var(--mask);
 
                     >p:nth-of-type(1){
                         font-family: 'Poppins', sans-serif;
-                        font-size: 14px;
+                        font-size: 12px;
                         font-weight: 700;
                         color: #2E2D33;
                         margin-bottom: 5px;
@@ -419,7 +380,7 @@ const ContainerFlight = styled.div`
                         font-size: 20px;
                         font-weight: 700;
                         color: #2E2D33;
-                        margin-bottom: 25px;
+                        margin-bottom: 20px;
                     }
 
                     >button{
@@ -434,7 +395,6 @@ const ContainerFlight = styled.div`
                         font-weight: 700;
                         cursor: pointer;
                     }
-
                 }
             }
         }
